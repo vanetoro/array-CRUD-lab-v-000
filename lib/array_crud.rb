cf1093426d8a481ba13cffa70fd4272e4d36af99
Vanessa Toro <vanetoro84@gmail.com>
2018-03-22 16:30:19 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
+   return new_arr = []
   
 end
 
 def create_an_array
-  
+  return new_arr = ['vane', 'rachel', 'jen', 'sarita']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  return array << element
 end
 
 def add_element_to_start_of_array(array, element)
+  return array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
+  return array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
+  array.last
   
 end
